a2a2476bbf555d1ed21d57d0bbc9da00c1daf5cb
Behlül Samil Selcuk <215264789+behluelsamilselcuk@users.noreply.github.com>
2025-09-22 22:45:21 +0200

    Programmiere die Logik des Spiels

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+const btns = document.querySelectorAll('button');
+let p = document.querySelector('p');
+const winnerDiv = document.querySelector('div');
+const restartBtn = document.createElement('button');
+const body = document.querySelector('body');
 
-    function getComputerChoice() {
-        let randomNum = Math.floor(Math.random() * 9);
-        
-        let choice;
-        if (randomNum >= 1 && randomNum <= 3) {
-            choice = 'Schere';
-        } else if (randomNum >= 4 && randomNum <= 6) {
-            choice = 'Stein';
-        } else {
-            choice = 'Papier';
-        }
+restartBtn.textContent = 'Spiel neustarten!';
 
-        return choice;
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 9);
+    
+    let choice;
+    if (randomNum >= 1 && randomNum <= 3) {
+        choice = 'schere';
+    } else if (randomNum >= 4 && randomNum <= 6) {
+        choice = 'stein';
+    } else {
+        choice = 'papier';
     }
 
-    function getHumanChoice() {
-        let choice = prompt('Schere, Stein oder Papier?', '');
-        let lowChoice = choice.toLowerCase();
-
-        if (lowChoice === 'schere') {
-            return 'Schere';
-        } else if (lowChoice === 'stein') {
-            return 'Stein';
-        } else if (lowChoice === 'papier') {
-            return 'Papier';
-        } else {
-            return 'Keine gültige Eingabe';
-        }
-    }
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            return 'Unentschieden! Ihr habt beide das gleiche gewählt!';
-        } else if (
-            humanChoice === 'Schere' && computerChoice === 'Papier' ||
-            humanChoice === 'Papier' && computerChoice === 'Stein' ||
-            humanChoice === 'Stein' && computerChoice === 'Schere'
-        ) {
-            humanScore++;
-            return `Du gewinnst! ${humanChoice} schlägt ${computerChoice}!`;
-        } else {
-            computerScore++;
-            return `Du verlierst! ${computerChoice} schlägt ${humanChoice}!`;
-        }
-    }
-
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    return choice;
 }
 
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
+function playRound(e) {
+    const humanChoice = e.target.value;
+    const computerChoice = getComputerChoice();
+
+    if (humanChoice === computerChoice) {
+        p.textContent = 'Unentschieden! Ihr habt beide das gleiche gewählt!';
+    } else if (
+        humanChoice === 'schere' && computerChoice === 'papier' ||
+        humanChoice === 'papier' && computerChoice === 'stein' ||
+        humanChoice === 'stein' && computerChoice === 'schere'
+    ) {
+        humanScore++;
+        console.log(humanScore)
+        p.textContent = `Du gewinnst! ${humanChoice} schlägt ${computerChoice}!`;
+    } else {
+        computerScore++;
+        console.log(computerScore)
+        p.textContent = `Du verlierst! ${computerChoice} schlägt ${humanChoice}!`;
+    }
+
+    winnerDiv.textContent = `DU = ${humanScore} : Computer = ${computerScore}`;
+
+
+    if (humanScore === 5) {
+        btns.forEach(btn => {
+            btn.disabled = true;            
+        });
+        body.appendChild(restartBtn);
+    }
+    else if (computerScore === 5) {
+        btns.forEach(btn => {
+            btn.disabled = true;            
+        });
+        body.appendChild(restartBtn);
+    }
+}
+
+btns.forEach(btn => {
+    btn.addEventListener('click', playRound)
+})
+
+restartBtn.addEventListener('click', () => {
+    window.location.reload()
+})
